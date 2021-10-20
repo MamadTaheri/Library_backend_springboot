1553b68516233955c8c8e496b4cf6a03ca64e7e9
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-20 22:33:49 +0330

    add getAllusers method

--- src/main/java/com/example/Library_backend_springboot/controller/personController.java
+++ src/main/java/com/example/Library_backend_springboot/controller/personController.java
@@ -1,10 +1,13 @@
 package com.example.Library_backend_springboot.controller;
 
+import com.example.Library_backend_springboot.model.userModel;
 import com.example.Library_backend_springboot.service.userService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value="/api")
@@ -13,5 +16,9 @@ public class personController {
     @Autowired
     userService userServiceObject;
 
+    @GetMapping( value = "/users")
+    public ArrayList<userModel> users() {
+        return userServiceObject.getAllusers();
+    }
 
 }
--- src/main/java/com/example/Library_backend_springboot/service/userService.java
+++ src/main/java/com/example/Library_backend_springboot/service/userService.java
@@ -1,6 +1,11 @@
 package com.example.Library_backend_springboot.service;
 
+import com.example.Library_backend_springboot.model.userModel;
+
+import java.util.ArrayList;
+
 public interface userService {
 
+    public ArrayList<userModel> getAllusers();
 
 }
