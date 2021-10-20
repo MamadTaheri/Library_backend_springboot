ff6824dccbd60fbed65e7cdbf0780d3c16fd577b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-21 01:27:48 +0330

    create bookService interface

---
+++ src/main/java/com/example/Library_backend_springboot/service/bookService.java
@@ -0,0 +1,11 @@
+package com.example.Library_backend_springboot.service;
+
+import com.example.Library_backend_springboot.model.bookModel;
+
+import java.util.ArrayList;
+
+public interface bookService {
+
+    public ArrayList<bookModel> getAllBooks();
+
+}
