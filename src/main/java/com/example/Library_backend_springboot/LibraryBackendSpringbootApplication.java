d9e0ee3a03ff4eea4108f5d115c09c132959fcf9
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-20 20:50:57 +0330

    init commit

---
+++ src/main/java/com/example/Library_backend_springboot/LibraryBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Library_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackendSpringbootApplication.class, args);
+	}
+
+}
